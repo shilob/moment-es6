80cd70ee1d7c81cec23ac39a59f25d2ba7ce0380
Shilo B <shiloworks@gmail.com>
2017-06-08 15:43:08 +1000

    Added locale

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 "use strict"
-var moment = require("moment");
+var moment = require("moment/min/moment-with-locales");
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = moment;
